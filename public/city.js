edb4a4cbebb1c95b155ab1e5d285580d59c8a160
Felix <111679444+mooseRobot@users.noreply.github.com>
2023-11-02 21:05:57 -1000

    Reduce clock update timer to one second.

--- public/city.js
+++ public/city.js
@@ -13,7 +13,7 @@ function updateTime(){
     hours = hours ? hours : 12; // the hour '0' should be '12'
     minutes = minutes < 10 ? '0'+minutes : minutes;
     currentTime.textContent = (hours + ':' + minutes + ' ' + ampm);
-    setTimeout(updateTime, 15000);
+    setTimeout(updateTime, 1000);
 }
 
 
