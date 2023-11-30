42ed68f579cd072698dfc0836e65821682b1d3e2
bluestonewhitestone <89286297+bluestonewhitestone@users.noreply.github.com>
2023-11-30 15:53:21 -0800

    Update weather_api_microservice.py
    Updated get method to produce results for any city

--- weather_api_microservice.py
+++ weather_api_microservice.py
@@ -3,93 +3,83 @@ import requests
 from datetime import date, timedelta, datetime
 
 
-API_KEY = "40f6beb9a3d9d3d1d6801a4a00e1ea86"
-city = "Ventura"
-limit = ""
-
-geo_response = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit={limit}&appid={API_KEY}")
-
-lat = geo_response.json()[0]["lat"]
-lon = geo_response.json()[0]["lon"]
-
-
-temp = ""
-
-weather_response_current = requests.get(f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&units=imperial&&cnt={3}&appid={API_KEY}")
-
-
-# current weather forecast
-for k in range(len(weather_response_current.json()["list"])):
-
-    dt = (weather_response_current.json()["list"][k]["dt"])
-    new_date = (date.fromtimestamp(dt))
-    on_date = (str(new_date))[0:10]
-    print(on_date, "success")
-    if on_date != date.today():
-        on_date = date.today()
-        print(on_date, "success1")
-
-# from date/month/time to epoch time
-epoch = datetime(1970, 1, 1)
-d = datetime(int((str(on_date))[0:4]), int((str(on_date))[5:7]), int((str(on_date))[8::]))
-new_example = ((d - epoch).total_seconds())
-print(int(new_example))
-
-
-
-today = date.today()
-future_date_first = timedelta(days=1) + today
-future_date_last = timedelta(days=2) + today
-
-epoch = datetime(1970, 1, 1)
-d = datetime(int((str(future_date_first))[0:4]), int((str(future_date_first))[5:7]), int((str(future_date_first))[8::]))
-d_1 = datetime(int((str(future_date_last))[0:4]), int((str(future_date_last))[5:7]), int((str(future_date_last))[8::]))
-future = ((d - epoch).total_seconds())
-future_1 = ((d_1 - epoch).total_seconds())
-print(int(future))
-print(int(future_1))
-
-# previous dates
-today = date.today()
-prev_date_first = today - timedelta(days=1)
-prev_date_last = today - timedelta(days=2)
-print(prev_date_first)
-print(prev_date_last)
-
-epoch = datetime(1970, 1, 1)
-new_d = datetime(int((str(prev_date_first))[0:4]), int((str(prev_date_first))[5:7]), int((str(prev_date_first))[8::]))
-new_d1 = datetime(int((str(prev_date_last))[0:4]), int((str(prev_date_last))[5:7]), int((str(prev_date_last))[8::]))
-past = ((new_d - epoch).total_seconds())
-past_1 = ((new_d1 - epoch).total_seconds())
-print(int(past))
-print(int(past_1))
-
-
-
-weather_response_previous = requests.get(f"https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=daily&units=imperial&start={int(past_1)}&end={int(past)}&appid={API_KEY}")
-weather_response_future = requests.get(f"https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&units=imperial&start={int(future)}&end={int(future_1)}&appid={API_KEY}")
-weather_response_current_updated = requests.get(f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&units=imperial&&cnt={3}&appid={API_KEY}")
-
-
-
-
-
-icon = "put icon here"
-
-
-icon_url = f"http://openweathermap.org/img/w/{icon}.png"
-
-
-
-
-
-
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def prev():
+    API_KEY = "40f6beb9a3d9d3d1d6801a4a00e1ea86"
+    cityname = request.args.get("cityname")
+    limit = ""
+
+    geo_response = requests.get(
+        f"http://api.openweathermap.org/geo/1.0/direct?q={cityname}&limit={limit}&appid={API_KEY}")
+
+    lat = geo_response.json()[0]["lat"]
+    lon = geo_response.json()[0]["lon"]
+
+    temp = ""
+
+    weather_response_current = requests.get(
+        f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&units=imperial&&cnt={3}&appid={API_KEY}")
+
+    # current weather forecast
+    for k in range(len(weather_response_current.json()["list"])):
+
+        dt = (weather_response_current.json()["list"][k]["dt"])
+        new_date = (date.fromtimestamp(dt))
+        on_date = (str(new_date))[0:10]
+        print(on_date, "success")
+        if on_date != date.today():
+            on_date = date.today()
+            print(on_date, "success1")
+
+    # from date/month/time to epoch time
+    epoch = datetime(1970, 1, 1)
+    d = datetime(int((str(on_date))[0:4]), int((str(on_date))[5:7]), int((str(on_date))[8::]))
+    new_example = ((d - epoch).total_seconds())
+    print(int(new_example))
+
+    today = date.today()
+    future_date_first = timedelta(days=1) + today
+    future_date_last = timedelta(days=2) + today
+
+    epoch = datetime(1970, 1, 1)
+    d = datetime(int((str(future_date_first))[0:4]), int((str(future_date_first))[5:7]),
+                 int((str(future_date_first))[8::]))
+    d_1 = datetime(int((str(future_date_last))[0:4]), int((str(future_date_last))[5:7]),
+                   int((str(future_date_last))[8::]))
+    future = ((d - epoch).total_seconds())
+    future_1 = ((d_1 - epoch).total_seconds())
+    print(int(future))
+    print(int(future_1))
+
+    # previous dates
+    today = date.today()
+    prev_date_first = today - timedelta(days=1)
+    prev_date_last = today - timedelta(days=2)
+    print(prev_date_first)
+    print(prev_date_last)
+
+    epoch = datetime(1970, 1, 1)
+    new_d = datetime(int((str(prev_date_first))[0:4]), int((str(prev_date_first))[5:7]),
+                     int((str(prev_date_first))[8::]))
+    new_d1 = datetime(int((str(prev_date_last))[0:4]), int((str(prev_date_last))[5:7]), int((str(prev_date_last))[8::]))
+    past = ((new_d - epoch).total_seconds())
+    past_1 = ((new_d1 - epoch).total_seconds())
+    print(int(past))
+    print(int(past_1))
+
+    weather_response_previous = requests.get(
+        f"https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=daily&units=imperial&start={int(past_1)}&end={int(past)}&appid={API_KEY}")
+    weather_response_future = requests.get(
+        f"https://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&units=imperial&start={int(future)}&end={int(future_1)}&appid={API_KEY}")
+    weather_response_current_updated = requests.get(
+        f"https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&units=imperial&&cnt={3}&appid={API_KEY}")
+
+    icon = "put icon here"
+
+    icon_url = f"http://openweathermap.org/img/w/{icon}.png"
 
     previous_weather = {
         "previous_weather_2_day": {
